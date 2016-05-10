cdd50673a9e7107fe93272838be458e1aec90c0e
cvetanoskin <cvetanoskinino@gmail.com>
2016-05-10 16:55:31 +0200

    Add files via upload

---
+++ TestiranjeProba/src/newpackage/ProstBroj.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author W7
+ */
+public class ProstBroj {
+    public static boolean prost (int n){
+        for (int i=0;i<n;i++){
+            if(n%i != 0){
+                return false;
+            }
+            
+        }
+        return true;
+    }
+}
